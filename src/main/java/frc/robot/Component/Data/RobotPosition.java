b9daf1342d953b955230dfaa77069ec7cd924384
mickjosh <felix_roux@hotmail.com>
2020-01-16 13:24:33 -0500

    Adding Class To Estimate A Robot Position On The Field

---
+++ src/main/java/frc/robot/Component/Data/RobotPosition.java
@@ -0,0 +1,38 @@
+package frc.robot.Component.Data;
+
+import edu.wpi.first.wpilibj.drive.Vector2d;
+
+public class RobotPosition 
+{
+    public RobotPosition(Vector2d InitialPos)
+    {
+        _pos = InitialPos;
+    }
+
+    private Vector2d _pos;
+    private Vector2d _velocity;
+
+    public Vector2d GetPosition()
+    {
+        return _pos;
+    }
+    public Vector2d GetVelocity()
+    {
+        return _velocity;
+    }
+
+    public void Evaluate(Vector2d Acceleration, double DT)
+    {
+        _pos.x += (_velocity.x * DT) + 0.5 * (Acceleration.x * DT * DT);
+        _pos.y += (_velocity.y * DT) + 0.5 * (Acceleration.y * DT * DT);
+
+        _velocity.x += Acceleration.x * DT;
+        _velocity.y += Acceleration.y * DT;
+    }
+
+    public void Reset()
+    {
+        _pos = new Vector2d(0, 0);
+        _velocity = new Vector2d(0, 0);
+    }
+}
