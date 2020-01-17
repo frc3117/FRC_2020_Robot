d181d1b21826b9cdceee0dae81b6f6ca8c960e77
mickjosh <felix_roux@hotmail.com>
2020-01-17 12:29:40 -0500

    Adding Possibility To Create Curve From Lambda Expresion

--- src/main/java/frc/robot/Math/Curve.java
+++ src/main/java/frc/robot/Math/Curve.java
@@ -7,6 +7,8 @@
 
 package frc.robot.Math;
 
+import java.util.function.Function;
+
 import edu.wpi.first.wpilibj.drive.Vector2d;
 
 public class Curve 
@@ -14,6 +16,18 @@ public class Curve
     public Curve(Vector2d... Points)
     {
         _points = Points.clone();
+    }
+    public Curve(int res, double min, double max, Function<Double, Double> Equation)
+    {
+        _points = new Vector2d[res];
+
+        for(int i = 0; i < res; i++)
+        {
+            double x = Mathf.Lerp(min, max, i / (double)res);
+            double y = Equation.apply(x);
+
+            _points[i] = new Vector2d(x, y);
+        }
     }
 
     private Vector2d[] _points;
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import frc.robot.Component.BallIntake;
@@ -34,7 +27,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
-    
     //Initializing the SwerveDrive drivetrain
     WheelData[] data = {
       new WheelData(15, 7, 3, 3, new Vector2d(1, 1), 3.6768434- (3.1415/2.0)),
