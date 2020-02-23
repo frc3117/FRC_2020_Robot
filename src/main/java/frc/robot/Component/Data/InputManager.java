abd16efc0d9ce5d76cec92abbc778d5c3795ba95
mickjosh <felix_roux@hotmail.com>
2020-02-23 11:23:11 -0500

    Adding Callback For Joystick Button

--- src/main/java/frc/robot/Component/Data/InputManager.java
+++ src/main/java/frc/robot/Component/Data/InputManager.java
@@ -1,6 +1,8 @@
 package frc.robot.Component.Data;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * A class that contain the initialization of the input
@@ -12,6 +14,7 @@ public class InputManager
     private static HashMap<String, Boolean> _lastState = new HashMap<String, Boolean>();
     private static HashMap<String, Boolean> _currentState = new HashMap<String, Boolean>();
 
+    private static HashMap<String, List<ButtonCallback>> _buttonCallback = new HashMap<String, List<ButtonCallback>>();
     /**
      * Initialize the input of the project
      */
@@ -57,9 +60,77 @@ public class InputManager
     {
         for (String key : _allButton)
         {
-            _lastState.put(key, _currentState.get(key));
-            _currentState.put(key, Input.GetButton(key));
+            boolean last = _currentState.get(key);
+            boolean current = Input.GetButton(key);
+
+            _lastState.put(key, last);
+            _currentState.put(key, current);
+
+            if(current && _buttonCallback.containsKey(key))
+            {
+                for(ButtonCallback callback : _buttonCallback.get(key))
+                {
+                    callback.OnPressed();
+                }
+            }
+            if((current && !last) && _buttonCallback.containsKey("Down/" + key))
+            {
+                for(ButtonCallback callback : _buttonCallback.get("Down/" + key))
+                {
+                    callback.OnPressed();
+                }
+            }
+            else if((!current && last) && _buttonCallback.containsKey("Up/" + key))
+            {
+                for(ButtonCallback callback : _buttonCallback.get("Up/" + key))
+                {
+                    callback.OnPressed();
+                }
+            }
         }
+    }
+
+    /**
+     * Add a callback for a button when the button is pressed
+     * @param ButtonName The name of the button to add a callback to
+     * @param Callback The callback of the button
+     */
+    public static void AddButtonCallback(String ButtonName, ButtonCallback Callback)
+    {
+        if(!_buttonCallback.containsKey(ButtonName))
+        {
+            _buttonCallback.put(ButtonName, new ArrayList<ButtonCallback>());
+        }
+
+        _buttonCallback.get(ButtonName).add(Callback);
+    }
+    /**
+     * Add a callback for a button when the button is pressed down
+     * @param ButtonName The name of the button to add a callback to
+     * @param Callback The callback of the button
+     */
+    public static void AddButtonDownCallback(String ButtonName, ButtonCallback Callback)
+    {
+        if(!_buttonCallback.containsKey("Down/" + ButtonName))
+        {
+            _buttonCallback.put(ButtonName, new ArrayList<ButtonCallback>());
+        }
+
+        _buttonCallback.get("Down/" + ButtonName).add(Callback);
+    }
+    /**
+     * Add a callback for a button when the button is released
+     * @param ButtonName The name of the button to add a callback to
+     * @param Callback The callback of the button
+     */
+    public static void AddButtonUpCallback(String ButtonName, ButtonCallback Callback)
+    {
+        if(!_buttonCallback.containsKey("Up/" + ButtonName))
+        {
+            _buttonCallback.put(ButtonName, new ArrayList<ButtonCallback>());
+        }
+
+        _buttonCallback.get("Up/" + ButtonName).add(Callback);
     }
 
     /**
@@ -88,5 +159,10 @@ public class InputManager
     public static boolean GetButtonUp(String ButtonName)
     {
         return _lastState.get(ButtonName) && !_currentState.get(ButtonName);
+    }
+
+    public interface ButtonCallback
+    {
+        public void OnPressed();
     }
 }
