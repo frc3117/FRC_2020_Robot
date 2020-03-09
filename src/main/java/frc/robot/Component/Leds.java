cd54d33907edafb2ce80419055391a4e35380cae
mickjosh <felix_roux@hotmail.com>
2020-03-09 17:01:17 -0400

    Adding Color Cycle To The LED System

--- src/main/java/frc/robot/Component/Leds.java
+++ src/main/java/frc/robot/Component/Leds.java
@@ -1,8 +1,10 @@
 package frc.robot.Component;
+import java.util.HashMap;
 import java.util.Random;
 
 import frc.robot.Component.Data.SolenoidValve;
 import frc.robot.Interface.Component;
+import frc.robot.Math.Timer;
 
 public class Leds implements Component {
     private SolenoidValve green;
@@ -12,7 +14,8 @@ public class Leds implements Component {
     private String _color = "";
     private Integer _priority = 0;
 
-    private CycleKey[] _cycle;
+    private HashMap<String, ColorCycle> _cycle;
+    private double _startTime;
     private int _currentCycleIndex = 0;
     private boolean _isCycle = false;
 
@@ -25,22 +28,73 @@ public class Leds implements Component {
     public void Init() {
         _color = "off";
         _priority = 0;
-    }
 
-    public void SetColorCycle(String Cycle)
-    {
-
+        _cycle = new HashMap<String, ColorCycle>();
     }
 
     public void SetColor(String color, Integer priority, Integer newPriority) {
         if (priority >= _priority) {
             _priority = newPriority;
             _color = color;
+
+            if(_cycle.containsKey(color))
+            {
+                _startTime = Timer.GetCurrentTime();
+                _currentCycleIndex = 0;
+            }
         }
     }
 
-    public void DoSystem() {    
-        switch (_color) {
+    public void AddColorCycle(String CycleName, String Cycle)
+    {
+        if(_cycle.containsKey(CycleName))
+            return;
+
+        String[] Split = Cycle.split(":", 0);
+
+        String[] color = new String[Split.length];
+        double[] time = new double[Split.length];
+
+        for(int i = 0; i < Split.length; i++)
+        {
+            String[] data = Split[i].split("_", 0);
+
+            color[i] = data[0];
+            time[i] = Double.parseDouble(data[1]);
+        }
+
+        ColorCycle current = new ColorCycle();
+        current.Color = color;
+        current.Time = time;
+
+        _cycle.put(CycleName, current);
+    }
+
+    public void DoSystem() {  
+        String CurrentColor;
+
+        if(_cycle.containsKey(_color))
+        {
+            ColorCycle current = _cycle.get(_color);
+
+            if(current.Time[_currentCycleIndex] <= Timer.GetCurrentTime() - _startTime)
+            {
+                if(_currentCycleIndex++ == current.Time.length)
+                {
+                    _currentCycleIndex = 0;
+                }
+
+                _startTime = 0;
+            }
+
+            CurrentColor = current.Color[_currentCycleIndex];
+        }
+        else
+        {
+            CurrentColor = _color;
+        }
+        
+        switch (CurrentColor) {
             case "green":
                 green.SetState(true);
                 blue.SetState(false);
@@ -98,9 +152,9 @@ public class Leds implements Component {
         }
     }
 
-    private class CycleKey
+    private class ColorCycle
     {
-        public String Color;
-        public double Time;
+        public String[] Color;
+        public double[] Time;
     }
 }
